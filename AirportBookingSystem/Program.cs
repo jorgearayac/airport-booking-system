0198c0608a8a6fd78c0f9babe0cd922a0a814d92
jorgearayac <araya.cepeda.jorge@gmail.com>
2025-12-29 15:01:47 -0300

    Basic logic in Program.cs

--- AirportBookingSystem/Program.cs
+++ AirportBookingSystem/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var flights = FileStorage.LoadData<Flight>("Data/flights.json");
+
+if (flights.Count == 0)
+{
+    Console.WriteLine("No flights available.");
+}
+else
+{
+    foreach (var flight in flights)
+    {
+        Console.WriteLine($"{flight.Id} | {flight.DepartureCountry} → {flight.DestinationCountry} | {flight.DepartureDate} | ${flight.BasePrice}");
+    }
+}
